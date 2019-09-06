06391368a7edb22a7515982fbf2859beb3cb4f0b
Alex Stefanskyi <stefanskioleksi@gmail.com>
2019-09-07 00:26:27 +0300

    done

--- src/js/modules/bootstrap.js
+++ src/js/modules/bootstrap.js
@@ -2,4 +2,9 @@
 //=../../../node_modules/popper.js/dist/umd/popper.js
 //=../../../node_modules/bootstrap/js/dist/util.js
 //=../../../node_modules/bootstrap/js/dist/tooltip.js
+
+
+import '../../../node_modules/bootstrap/js/dist/collapse'
+
+
 //=../modules/custom/tooltips.js
