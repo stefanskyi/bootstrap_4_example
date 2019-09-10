3708d601a56ef24e0b9f5f8496b40395ef3cd164
Alex Stefanskyi <stefanskioleksi@gmail.com>
2019-09-10 23:57:38 +0300

    add hash

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,118 +1,159 @@
-const path=require('path')
-const webpack=require('webpack')
+const path = require('path')
+const webpack = require('webpack')
+// const fs = require('fs')
 
 
-const MiniCssExtractPlugin=require('mini-css-extract-plugin')
-const CopyWebpackPlugin=require('copy-webpack-plugin')
-const HtmlWebpackPlugin=require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const PATHS={
+const PATHS = {
     src: path.join(__dirname, '../src'),
     dist: path.join(__dirname, '../dist'),
     assets: 'assets/'
+
 }
 
-module.exports={
-    externals:{
-        paths:PATHS
+// Pages const for HtmlWebpackPlugin
+// see more: https://github.com/vedees/webpack-template/blob/master/README.md#html-dir-folder
+// const PAGES_DIR = PATHS.src
+// const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.html'))
+
+module.exports = {
+    externals: {
+        paths: PATHS
     },
-    entry:{
-        app:PATHS.src,
+    entry: {
+        app: PATHS.src,
+        // lk: `${PATHS.src}/lk.js` вариант второй точки входа
     },
-    output:{
-        filename:`${PATHS.assets}js/[name].js`,
-        path:PATHS.dist,
-        publicPath:''
+    output: {
+        filename: `${PATHS.assets}js/[name].[hash].js`,
+        path: PATHS.dist,
+        publicPath: '/'
     },
-    module:{
-        rules:[{
-            test:/\.js$/,
-            loader:'babel-loader',
-            exclude:'/node_modules'
-        },{
-            test:/\.(gif|png|jpe?g|svg)$/i,
-            use:[
-                {
-                    loader:'file-loader',
-                    options:{
-                        name:'[name].[ext]',
-                        outputPath: './assets/img',
-                        // outputPath: './assets/img' это удалить если не заведется
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    name: 'vendors',
+                    test: /node_modules/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules'
+        }, {
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: './assets/img',
+                    useRelativePath: true
+                    // outputPath: './assets/img' тут норм, теперь кидает куда надо
+                }
+            }, {
+                loader: 'image-webpack-loader',
+                options: {
+                    mozjpeg: {
+                        progressive: true,
+                        quality: 65
+                    },
+                    // optipng.enabled: false will disable optipng
+                    optipng: {
+                        enabled: false,
+                    },
+                    pngquant: {
+                        quality: '65-90',
+                        speed: 4
+                    },
+                    gifsicle: {
+                        interlaced: false,
+                    },
+                    // the webp option will enable WEBP
+                    webp: {
+                        quality: 75
                     }
-                },{
-                    loader: 'image-webpack-loader',
+                }
+            }]
+        }, {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
                     options: {
-                        mozjpeg: {
-                            progressive: true,
-                            quality: 65
-                        },
-          // optipng.enabled: false will disable optipng
-                        optipng: {
-                            enabled: false,
-                        },
-                        pngquant: {
-                            quality: '65-90',
-                            speed: 4
-                        },
-                        gifsicle: {
-                            interlaced: false,
-                        },
-          // the webp option will enable WEBP
-                        webp: {
-                            quality: 75
+                        sourceMap: true
+                    }
+                }, {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true,
+                        config: {
+                            path: `${PATHS.src}/js/postcss.config.js`
                         }
                     }
+                }, {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
                 }
             ]
-        },{
-            test:/\.scss$/,
-            use:[
+        }, {
+            test: /\.css$/,
+            use: [
                 'style-loader',
                 MiniCssExtractPlugin.loader,
                 {
-                    loader:'css-loader',
-                    options:{sourceMap:true}
-                },{
-                    loader:'postcss-loader',
-                    options:{sourceMap:true, config:{ path:`${PATHS.src}/js/postcss.config.js`}}
-                },{
-                    loader:'sass-loader',
-                    options:{sourceMap:true}
-                }
-            ]
-        },{
-            test:/\.css$/,
-            use:[
-                'style-loader',
-                MiniCssExtractPlugin.loader,
-                {
-                    loader:'css-loader',
-                    options:{sourceMap:true}
-                },{
-                    loader:'postcss-loader',
-                    options:{sourceMap:true, config:{ path:`${PATHS.src}/postcss.config.js`}}
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true,
+                        config: {
+                            path: `${PATHS.src}/postcss.config.js`
+                        }
+                    }
                 },
             ]
         }]
     },
     plugins: [
         new webpack.ProvidePlugin({
-            $:'jquery',
-            jQuery:'jquery',
-            jquery:'jquery',
+            $: 'jquery',
+            jQuery: 'jquery',
+            jquery: 'jquery',
             Popper: ['popper.js', 'default']
         }),
         new MiniCssExtractPlugin({
-          filename:`${PATHS.assets}css/[name].css`,
+            filename: `${PATHS.assets}css/[name].[hash].css`,
         }),
         new HtmlWebpackPlugin({
-            hash:false,
-            template:`${PATHS.src}/index.html`,
-            filename:'./index.html'
+            hash: false,
+            template: `${PATHS.src}/index.html`,
+            filename: './index.html',
+            // inject: false  убирает автоподставление css & js
         }),
-        new CopyWebpackPlugin([
-            {from:`${PATHS.src}/assets/img`,to:`${PATHS.assets}img`},
-            {from:`${PATHS.src}/static`,to:''},
+        new CopyWebpackPlugin([{
+                from: `${PATHS.src}/assets/img`,
+                to: `${PATHS.assets}img`
+            },
+            {
+                from: `${PATHS.src}/static`,
+                to: ''
+            },
         ]),
-      ],
+    ],
 }
